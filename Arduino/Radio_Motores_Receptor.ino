c33698551efb2c56fe2d875b9138116704e11889
Carlos Tonchee <carlostonchee92@gmail.com>
2020-09-05 16:14:27 -0500

    Emisor Receptor con RF24

---
+++ Arduino/Radio_Motores_Receptor.ino
@@ -0,0 +1,76 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+//variables de Radio.
+const int CE_PIN = 9;
+const int CSN_PIN = 10;
+
+byte direccion[5] = {'c','a','n','a','l'};
+RF24 radio(CE_PIN,CSN_PIN);
+char dato;
+
+void setup() {
+  // put your setup code here, to run once:
+  radio.begin();
+  radio.openReadingPipe(1,direccion);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+  analogWrite(6,0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  uint8_t numero_canal;
+  if(radio.available())
+  {
+    radio.read(dato,sizeof(dato));
+    switch(dato)
+    {
+      case 'f':
+        digitalWrite(2,HIGH);
+        digitalWrite(3,LOW);
+        digitalWrite(4,LOW);
+        digitalWrite(5,LOW);
+        analogWrite(6,90);
+      break;
+      case 'b':
+        digitalWrite(2,LOW);
+        digitalWrite(3,HIGH);
+        digitalWrite(4,LOW);
+        digitalWrite(5,LOW);
+        analogWrite(6,90);
+      break;
+      case 'l':
+        digitalWrite(2,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(4,HIGH);
+        digitalWrite(5,LOW);
+        analogWrite(6,90);
+      break;
+      case 'r':
+        digitalWrite(2,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(4,LOW);
+        digitalWrite(5,HIGH);
+        analogWrite(6,90);
+      break;
+    }
+  }
+  else
+  {
+    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    digitalWrite(5,LOW);
+    analogWrite(6,0);
+  }
+  delay(1000);
+}
---
+++ Arduino/Radio___Keypad.ino
@@ -0,0 +1,49 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Keypad.h>
+
+// declaracion de variables para la Radio
+const int CE_PIN  = 9;
+const int CSN_PIN = 10;
+
+byte direccion[6] = "00001";
+RF24 radio(CE_PIN, CSN_PIN);
+char dato;
+////////////
+const byte ROWS = 2;
+const byte COLS = 2;
+
+char keys[ROWS][COLS] = {
+  {'f' , 'b'},
+  {'l' , 'r'},
+};
+
+byte rowPins[ROWS] = {2,3};
+byte colPins[COLS] = {4,5}; 
+Keypad keypad = Keypad( makeKeymap(keys),rowPins,colPins,ROWS,COLS);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(direccion);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  
+  if(radio.write(dato,sizeof(dato)))
+  {
+    char key = keypad.getKey();
+    Serial.print("Dato Enviado : ");
+    Serial.println(key);
+  }
+  else
+  {
+    Serial.println("No se ha podido enviar");
+  }
+  delay(1000);
+}
